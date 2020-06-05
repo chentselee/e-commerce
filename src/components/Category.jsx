fdd1e4c66f1e0e5ab3cb52017fb2a24a427f9b33
承澤李 <chentselee@gmail.com>
2020-06-05 17:22:41 +0800

    Added badge to show numbers of products

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
+import Badge from "react-bootstrap/Badge";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "./Category.css";
 
-const Category = ({ categories }) => {
+const Category = () => {
+  const categories = useSelector((state) => state.categories);
   return (
     <ListGroup className="__aside">
       {categories.map((category) => (
-        <ListGroup.Item key={category.path}>
-          <Link to={category.path}>{category.display_name}</Link>
+        <ListGroup.Item key={category.name}>
+          <Link to={category.name}>{category.display_name}</Link>
+          <Badge pill variant="primary" className="__category-num-badge">
+            {category.num_products}
+          </Badge>
         </ListGroup.Item>
       ))}
     </ListGroup>
