913a545af14656b2762bcd99672e2712070ecf8e
承澤李 <chentselee@gmail.com>
2020-06-06 11:33:03 +0800

    Added title

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const About = () => {
   return (
     <div className="__main">
-      This is a demo e-commerce website for personal portfolio.
+      <h1>About</h1>
+      <p>This is a demo e-commerce website for personal portfolio.</p>
     </div>
   );
 };
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
-const About = () => {
+const Contact = () => {
   return (
     <div className="__main">
-      If you have any questions regarding the website, please contact me through{" "}
-      <a href="mailto:chentselee@gmail.com">this email</a>.
+      <h1>Contact</h1>
+      <p>
+        If you have any questions regarding the website, please contact me
+        through <a href="mailto:chentselee@gmail.com">this email</a>
+        <span> or visit the </span>
+        <a href="https://github.com/chentselee/e-commerce">github</a> page for
+        this project.
+      </p>
     </div>
   );
 };
 
-export default About;
+export default Contact;
