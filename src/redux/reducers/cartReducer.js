46c1b0bb1836ae8d6d4675c7f8c06141f0fbb753
承澤李 <chentselee@gmail.com>
2020-06-06 11:41:44 +0800

    Persist cart state to session storage

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -1,9 +1,12 @@
 import * as actionTypes from "../actionTypes";
+import { saveToSession, loadFromSession } from "./rootReducer";
 
-export default (state = [], action) => {
+const KEY = "cart";
+let newState;
+export default (state = loadFromSession(KEY), action) => {
   switch (action.type) {
     case actionTypes.ADD_TO_CART:
-      return [
+      newState = [
         ...state,
         {
           id: action.payload.id,
@@ -12,17 +15,23 @@ export default (state = [], action) => {
           amount: 1,
         },
       ];
+      saveToSession(KEY, newState);
+      return newState;
 
     case actionTypes.CHANGE_AMOUNT_IN_CART:
-      return state.map((item) => {
+      newState = state.map((item) => {
         if (item.id !== action.payload.id) return item;
         return {
           ...item,
           amount: action.payload.amount,
         };
       });
+      saveToSession(KEY, newState);
+      return newState;
     case actionTypes.REMOVE_FROM_CART:
-      return state.filter((item) => item.id !== action.payload.id);
+      newState = state.filter((item) => item.id !== action.payload.id);
+      saveToSession(KEY, newState);
+      return newState;
     default:
       return state;
   }
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -3,6 +3,22 @@ import cartReducer from "./cartReducer";
 import categoriesReducer from "./categoriesReducer";
 import productsReducer from "./productsReducer";
 
+export function saveToSession(key, data) {
+  try {
+    sessionStorage.setItem(key, JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export function loadFromSession(key) {
+  try {
+    return JSON.parse(sessionStorage.getItem(key));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 export default combineReducers({
   cart: cartReducer,
   categories: categoriesReducer,
