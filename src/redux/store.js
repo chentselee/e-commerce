ca721dd61395b874bf48b4b6a53ac875b523b5ee
承澤李 <chentselee@gmail.com>
2020-06-11 14:33:28 +0800

    Fixed devtool setting

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,7 +10,8 @@ export default createStore(
   compose(
     applyMiddleware(thunk),
     process.env.NODE_ENV === "development" &&
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__()
+      window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : ""
   )
 );
