4b5b9f4792e4ddfc93831a6f6ceadc7d2192e86d
承澤李 <chentselee@gmail.com>
2020-06-15 02:00:55 +0800

    Added spinner for sidebar

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,20 +3,25 @@ import ListGroup from "react-bootstrap/ListGroup";
 import Badge from "react-bootstrap/Badge";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import Spinner from "react-bootstrap/Spinner";
 import "./Category.css";
 
 const Category = () => {
   const categories = useSelector((state) => state.categories);
   return (
     <ListGroup className="__aside">
-      {categories.map((category) => (
-        <ListGroup.Item key={category.name}>
-          <Link to={"/" + category.name}>{category.display_name}</Link>
-          <Badge pill variant="primary" className="__category-num-badge">
-            {category.num_products}
-          </Badge>
-        </ListGroup.Item>
-      ))}
+      {categories.length ? (
+        categories.map((category) => (
+          <ListGroup.Item key={category.name}>
+            <Link to={"/" + category.name}>{category.display_name}</Link>
+            <Badge pill variant="primary" className="__category-num-badge">
+              {category.num_products}
+            </Badge>
+          </ListGroup.Item>
+        ))
+      ) : (
+        <Spinner animation="border" variant="primary" />
+      )}
     </ListGroup>
   );
 };
