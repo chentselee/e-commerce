e8c04b15199e932557adc3a4f996071f4e67a949
承澤李 <chentselee@gmail.com>
2020-06-19 04:23:50 +0800

    Replace replaceAll with regex that works on all browsers

--- src/components/ProductLink.jsx
+++ src/components/ProductLink.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const ProductLink = ({ category, name }) => {
-  return <Link to={category + "/" + name.replaceAll(" ", "-")}>{name}</Link>;
+  return <Link to={category + "/" + name.replace(/\s/g, "-")}>{name}</Link>;
 };
 
 export default ProductLink;
